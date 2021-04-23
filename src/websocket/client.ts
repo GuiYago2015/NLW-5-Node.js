2f62611c8a9e735acbb07c4f7357609b175bc691
Guilherme Yago <guiyago2015@gmail.com>
2021-04-23 13:29:44 -0300

    aula final - parte 1

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -4,7 +4,7 @@ import { UsersService} from "../services/UsersService";
 import { MessagesService} from "../services/MessagesSerive";
 
 interface IParams{
-    text: string,
+    text?: string,
     email: string;
 }
 
@@ -12,15 +12,11 @@ io.on("connect", (socket) => {
     const connectionsService = new ConnectionsService();
     const usersService = new UsersService();
     const messagesService = new MessagesService;
-
-
     socket.on("client_first_access", async (params) => {
         const socket_id = socket.id;
 
         const {text, email} = params as IParams;
         let user_id = null;
-
-
         const userExists = await usersService.findByEmail(email);
 
         if(!userExists){
@@ -44,15 +40,16 @@ io.on("connect", (socket) => {
 
                 await connectionsService.create(connection);
             }
-
-            
         }
 
 
         await messagesService.create({
             text,
             user_id
-        })
-        // salvar a conexao com o socket_id, user_id
+        });
+       
+        const allMessages = await messagesService.listByUser(user_id);
+
+        socket.emit("client_list_all_messages", allMessages);
     });
 });
